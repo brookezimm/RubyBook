b0deca9a94e9e9c5088a4f5a1161a2d9fdf925e9
brookezimm <brooklynn.zimmerman@gmail.com>
2020-05-15 21:07:04 -0500

    Edited exercise 1 in Prep

--- Prep/Exercise1.rb
+++ Prep/Exercise1.rb
@@ -3,6 +3,6 @@ cd my_folder
 touch one.rb
 touch two.rb
 echo "puts 'this is file one'" >> one.rb
-echo "puts 'this is file one'" >> one.rb
+echo "puts 'this is file two'" >> two.rb
 ruby one.rb
 ruby two.rb
